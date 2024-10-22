0675a9246cccfa506ff3d834b8398a452357f84c
alireza <alirezatt705@gmail.com>
2024-10-22 05:48:46 +0330

    key added to social media

--- src/app/components/contact-us-page/contact-us-social-media/contact-us-social-media.tsx
+++ src/app/components/contact-us-page/contact-us-social-media/contact-us-social-media.tsx
@@ -9,7 +9,7 @@ const ContactUsSocialMedia = (props: Props) => {
   return (
     <div className="bg-primary-main flex-center gap-5 rounded-2xl py-2 px-3 w-fit">
       {socialMediaItems.map((media: SocialItemType) => {
-        return <SocialMediaItem media={media} />;
+        return <SocialMediaItem key={media.title} media={media} />;
       })}
     </div>
   );
