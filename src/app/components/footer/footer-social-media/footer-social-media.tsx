853d3c484d2844eb186cccbffe4f9a863059c112
alireza <alirezatt705@gmail.com>
2024-10-22 05:52:10 +0330

    key added to SocialMediaItem

--- src/app/components/footer/footer-social-media/footer-social-media.tsx
+++ src/app/components/footer/footer-social-media/footer-social-media.tsx
@@ -10,7 +10,7 @@ const FooterSocialMedia = ({ socialMediaItems }: Props) => {
   return (
     <div className="bg-primary-main flex-center gap-5 rounded-b-2xl py-3 px-4 w-fit mx-auto">
       {socialMediaItems.map((media: SocialItemType) => {
-        return <SocialMediaItem media={media} />;
+        return <SocialMediaItem key={media.title} media={media} />;
       })}
     </div>
   );
