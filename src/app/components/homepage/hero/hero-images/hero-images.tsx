7ad1b9b30ec09c11d40726b8566aa3cd7daa492b
alireza <alirezatt705@gmail.com>
2024-10-25 20:26:30 +0330

    hero section changed

--- src/app/components/homepage/hero/hero-images/hero-images.tsx
+++ src/app/components/homepage/hero/hero-images/hero-images.tsx
@@ -1,112 +1,3 @@
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { motion } from "framer-motion";
-import { Product } from "../../../../../../next-type-models";
-
-type Props = {
-  products: Product[];
-  current: number;
-  changeSlide: (index: number) => void;
-};
-
-const HeroImages = ({ products, current, changeSlide }: Props) => {
-
-
-  const getSlideInVariant = (index: number) => {
-    switch (index) {
-      case 0:
-        return {
-          hidden: { x: -200, y: -200, opacity: 0, scale: 0.6 }, // Top-left
-          visible: { x: [-200, 20, 0], y: 0, opacity: 1, scale: [0.6, 1.2, 1] },
-          exit: { x: 200, y: -200, opacity: 0 }, // Move out to the right
-        };
-      case 1:
-        return {
-          hidden: { x: -200, y: 0, opacity: 0, scale: 0.6 }, // Center-left
-          visible: { x: [-200, 20, 0], y: 0, opacity: 1, scale: [0.6, 1.2, 1] },
-          exit: { x: 200, y: 0, opacity: 0 }, // Move out to the right
-        };
-      case 2:
-        return {
-          hidden: { x: -200, y: 200, opacity: 0, scale: 0.6 }, // Bottom-left
-          visible: { x: [-200, 20, 0], y: 0, opacity: 1, scale: [0.6, 1.2, 1] },
-          exit: { x: 200, y: 200, opacity: 0 }, // Move out to the right
-        };
-      default:
-        return {
-          hidden: { x: -200, opacity: 0, scale: 0.6 },
-          visible: { x: [-200, 20, 0], opacity: 1, scale: [0.6, 1.2, 1] },
-          exit: { x: 200, opacity: 0 },
-        };
-    }
-  };
-
-  return (
-    <div className="flex justify-between w-[95%] mx-auto sm:max-w-[500px] md:w-6/12 md:mr-0 md:max-w-none">
-      <Link
-        href={`products/${products[current].id}`}
-        className="w-1/2 aspect-square relative bg-primary-light rounded-full h-fit my-auto"
-      >
-        <motion.div
-          key={products[current].id}
-          variants={getSlideInVariant(current)} // Dynamically set animation based on the index
-          initial="hidden" // Initial state (off-screen left)
-          animate="visible" // Animate to visible state
-          exit="exit" // When exiting, animate out to the right
-          transition={{ duration: 1 }} // Control the duration of the animation
-          className="w-full h-full relative"
-        >
-          <Image
-            alt={products[current].title}
-            src={products?.[current]?.images?.[0]?.url!}
-            fill
-            style={{
-              objectFit: "cover",
-              scale: "1.2",
-            }}
-          />
-        </motion.div>
-      </Link>
-      <div className="flex flex-col w-1/2 relative">
-        {products.map((product, index) => (
-          <div
-            key={product.id}
-            onClick={() => changeSlide(index)}
-            className={`${
-              current === index ? "bg-primary-main" : "bg-primary-light"
-            } aspect-square cursor-pointer rounded-2xl custom-transition hover:opacity-60 w-6/12 absolute 
-               ${
-                 index === 0
-                   ? "-top-[30%] md:top-[5%] bmlg:top-0 blgxl:-top-[15%]"
-                   : index === 1
-                   ? "absolute-vertically-center right-[30%]"
-                   : index === 2
-                   ? "-bottom-[30%] md:bottom-[5%] bmlg:bottom-0 blgxl:-bottom-[15%]"
-                   : ""
-               }
-            `}
-          >
-            <Image
-              alt={product.title}
-              src={product?.images?.[0]?.url!}
-              fill
-              style={{
-                objectFit: "cover",
-                scale: "1.2",
-              }}
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default HeroImages;
-
-
-
 // import React from "react";
 // import Image from "next/image";
 // import Link from "next/link";
@@ -120,6 +11,8 @@ export default HeroImages;
 // };
 
 // const HeroImages = ({ products, current, changeSlide }: Props) => {
+
+
 //   const getSlideInVariant = (index: number) => {
 //     switch (index) {
 //       case 0:
@@ -153,7 +46,7 @@ export default HeroImages;
 //     <div className="flex justify-between w-[95%] mx-auto sm:max-w-[500px] md:w-6/12 md:mr-0 md:max-w-none">
 //       <Link
 //         href={`products/${products[current].id}`}
-//         className="w-1/2 aspect-square relative bg-primary-light rounded-full h-fit my-auto overflow-hidden"
+//         className="w-1/2 aspect-square relative bg-primary-light rounded-full h-fit my-auto"
 //       >
 //         <motion.div
 //           key={products[current].id}
@@ -170,6 +63,7 @@ export default HeroImages;
 //             fill
 //             style={{
 //               objectFit: "cover",
+//               scale: "1.2",
 //             }}
 //           />
 //         </motion.div>
@@ -181,7 +75,7 @@ export default HeroImages;
 //             onClick={() => changeSlide(index)}
 //             className={`${
 //               current === index ? "bg-primary-main" : "bg-primary-light"
-//             } overflow-hidden aspect-square cursor-pointer rounded-2xl custom-transition hover:opacity-60 w-6/12 absolute 
+//             } aspect-square cursor-pointer rounded-2xl custom-transition hover:opacity-60 w-6/12 absolute 
 //                ${
 //                  index === 0
 //                    ? "-top-[30%] md:top-[5%] bmlg:top-0 blgxl:-top-[15%]"
@@ -199,6 +93,7 @@ export default HeroImages;
 //               fill
 //               style={{
 //                 objectFit: "cover",
+//                 scale: "1.2",
 //               }}
 //             />
 //           </div>
@@ -209,3 +104,108 @@ export default HeroImages;
 // };
 
 // export default HeroImages;
+
+
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { Product } from "../../../../../../next-type-models";
+
+type Props = {
+  products: Product[];
+  current: number;
+  changeSlide: (index: number) => void;
+};
+
+const HeroImages = ({ products, current, changeSlide }: Props) => {
+  const getSlideInVariant = (index: number) => {
+    switch (index) {
+      case 0:
+        return {
+          hidden: { x: -200, y: -200, opacity: 0, scale: 0.6 }, // Top-left
+          visible: { x: [-200, 20, 0], y: 0, opacity: 1, scale: [0.6, 1.2, 1] },
+          exit: { x: 200, y: -200, opacity: 0 }, // Move out to the right
+        };
+      case 1:
+        return {
+          hidden: { x: -200, y: 0, opacity: 0, scale: 0.6 }, // Center-left
+          visible: { x: [-200, 20, 0], y: 0, opacity: 1, scale: [0.6, 1.2, 1] },
+          exit: { x: 200, y: 0, opacity: 0 }, // Move out to the right
+        };
+      case 2:
+        return {
+          hidden: { x: -200, y: 200, opacity: 0, scale: 0.6 }, // Bottom-left
+          visible: { x: [-200, 20, 0], y: 0, opacity: 1, scale: [0.6, 1.2, 1] },
+          exit: { x: 200, y: 200, opacity: 0 }, // Move out to the right
+        };
+      default:
+        return {
+          hidden: { x: -200, opacity: 0, scale: 0.6 },
+          visible: { x: [-200, 20, 0], opacity: 1, scale: [0.6, 1.2, 1] },
+          exit: { x: 200, opacity: 0 },
+        };
+    }
+  };
+
+  return (
+    <div className="flex justify-between w-[95%] mx-auto sm:max-w-[500px] md:w-6/12 md:mr-0 md:max-w-none">
+      <Link
+        href={`products/${products[current].id}`}
+        className="w-1/2 aspect-square relative bg-primary-light rounded-full h-fit my-auto overflow-hidden border-4 border-primary-main"
+      >
+        <motion.div
+          key={products[current].id}
+          variants={getSlideInVariant(current)} // Dynamically set animation based on the index
+          initial="hidden" // Initial state (off-screen left)
+          animate="visible" // Animate to visible state
+          exit="exit" // When exiting, animate out to the right
+          transition={{ duration: 1 }} // Control the duration of the animation
+          className="w-full h-full relative"
+        >
+          <Image
+            alt={products[current].title}
+            src={products?.[current]?.images?.[0]?.url!}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </motion.div>
+      </Link>
+      <div className="flex flex-col w-1/2 relative">
+        {products.map((product, index) => (
+          <div
+            key={product.id}
+            onClick={() => changeSlide(index)}
+            className={`${
+              current === index ? "bg-primary-main" : "bg-primary-light"
+            } overflow-hidden border-4 border-primary-main aspect-square cursor-pointer rounded-2xl custom-transition hover:opacity-60 w-6/12 absolute 
+               ${
+                 index === 0
+                   ? "-top-[30%] md:top-[5%] bmlg:top-0 blgxl:-top-[15%]"
+                   : index === 1
+                   ? "absolute-vertically-center right-[30%]"
+                   : index === 2
+                   ? "-bottom-[30%] md:bottom-[5%] bmlg:bottom-0 blgxl:-bottom-[15%]"
+                   : ""
+               }
+            `}
+          >
+            <Image
+              alt={product.title}
+              src={product?.images?.[0]?.url!}
+              fill
+              style={{
+                objectFit: "cover",
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HeroImages;
