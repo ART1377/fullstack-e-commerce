2599c045769859a12097326fdb1a169522454bf2
alireza <alirezatt705@gmail.com>
2024-11-06 20:05:46 +0330

    lazy loading added to one image for test

--- src/app/components/homepage/category/category.tsx
+++ src/app/components/homepage/category/category.tsx
@@ -56,6 +56,7 @@ const Category = (props: Props) => {
             style={{
               objectFit: "cover",
             }}
+            loading="lazy"
           />
         </div>
       </section>
